c3b1bc7a3528c6ad1a66244ee3281a62b7d53f7a
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-15 18:32:46 +0300

    Adding css on App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from './Header';
 import Content from './Content';
 
+// import style.css stylesheet
+import './style.css'
+
 class App extends React.Component{
 
   // Multiple Line items need a wrapper 
   render(){
 
     return(
-      <div>
+      <div className="myStyle">
          <Header />
          <Content />
       </div>
