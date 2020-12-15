31af9ddd7ba8e6d0ec71bc1145d55013b7b95cd6
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-15 13:20:32 +0300

    Apply style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ class App extends React.Component{
   // Multiple Line items need a wrapper 
   render(){
     let i = 1
+    let style = {
+      fontSize : 50 , 
+      color : "red"
+    }
     return(
       <div>
          <h1>Header !!</h1>
@@ -12,7 +16,10 @@ class App extends React.Component{
         <p data-myattribute="Some Value">This is the Content !!! </p>
         {/* Expression  */}
         <p>This is Expression Adding Two Numbers : {1 + 1}</p>
-        <p>Conditional Statement => {i==1 ? "True" : "False"}</p>
+        <p>Conditional Statement = {i==1 ? "True" : "False"}</p>
+
+        {/* Applying Inline Style  */}
+        <p style={style}>Apply Style </p>
       </div>
     );
   }
