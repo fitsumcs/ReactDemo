201ebcae2884ac9a652a56b386df9ee0c8ea18bb
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-15 13:17:06 +0300

    Adding Expression with Conditional

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ class App extends React.Component{
 
   // Multiple Line items need a wrapper 
   render(){
+    let i = 1
     return(
       <div>
          <h1>Header !!</h1>
          <h2>Content</h2>
         <p data-myattribute="Some Value">This is the Content !!! </p>
+        {/* Expression  */}
+        <p>This is Expression Adding Two Numbers : {1 + 1}</p>
+        <p>Conditional Statement => {i==1 ? "True" : "False"}</p>
       </div>
     );
   }
